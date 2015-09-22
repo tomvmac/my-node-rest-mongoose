5be44c1b0d538fb120ef9d126a997a8d6fddd539
unknown <tmac@tmac-win7pc.crunchtime.local>
2015-09-22 09:01:12 -0400

    commit personModel

---
+++ models/personModel.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+/* MongoDb Database Connection */
+
+var uristring = 'mongodb://localhost/tomdb';
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+var Schema = mongoose.Schema;
+
+// create a schema
+var personSchema = new Schema({
+  personId: Number,
+  firstName: String,
+  lastName: String,
+  age: Number,    
+  sex: String
+});
+
+// Create a person model from schema
+var personModel = mongoose.model('personModel', personSchema, 'person');
+
+module.exports = personModel;
