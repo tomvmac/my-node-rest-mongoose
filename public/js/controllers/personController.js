7315947e1835c6e58abbf1ec81eaad6493d5d966
unknown <tmac@tmac-win7pc.crunchtime.local>
2015-09-28 16:47:08 -0400

    Refactored angular personController to include methods for get and delete

--- public/js/controllers/personController.js
+++ public/js/controllers/personController.js
@@ -1,18 +1,37 @@
 var mainApp = angular.module("mainApp", []);
 
-mainApp.controller("personController", function($scope, $http) {
-    
-    // Init
-    var url = "/api/person";
+mainApp.controller("personController", function ($scope, $http) {
 
-    $http.get(url).success(function (response) {
-        $scope.persons = response;
-    });
-    
     // Methods
-    $scope.sayHello = function (myName) {
-        alert('Hello ' + myName);
-    }
-    
-    
+
+    // getPersons
+    $scope.getPersons = function () {
+        var url = "/api/person";
+        var tempResponse;
+
+        $http.get(url).success(function (response) {
+            $scope.persons = response;
+        });
+        return $scope.persons;
+    };
+
+    // deletePerson
+    $scope.deletePerson = function (personObjectId) {
+
+        var url = "/api/person/" + personObjectId;
+        alert(url);
+        $http.delete(url).success(function (response) {
+            // re-fetch Persons    
+            $scope.getPersons();
+        });
+
+    };
+
+
+
+    // Init
+    $scope.getPersons();
+
+
+
 });
